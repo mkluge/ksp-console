6781ce8576b91487999d2a557d2c780afaf39b1a
Michael Kluge <vollseil@arcor.de>
2016-07-01 22:06:23 +0200

    noch zu groß ...

--- display_controller/display_controller.ino
+++ display_controller/display_controller.ino
@@ -1,196 +1,176 @@
 #include "Arduino.h"
 #include "ArduinoJson.h"
-#include "LedControl.h"
 #include "UTFT.h"
 #include "Wire.h"
 #include "../main_controller/ConsoleSetup.h"
 
-// Declare which fonts we will be using
-//extern uint8_t SmallFont[];
-extern uint8_t BigFont[];
-//extern uint8_t SevenSegNumFont[];
-
-// Belegung für braun = 21
- #define SCLK2 17
- #define MOSI2 18
- #define CS2   21
- #define DC2   19
- #define RESET2 20
-
-// Belegung für braun = 52
-#define SCLK1 44
-#define MOSI1 46
-#define CS1   52
-#define DC1   48
-#define RESET1 50
+// declare font
+extern uint8_t SmallFont[];
 
 //UTFT(Model, SDA, SCL, CS, RST[, RS]);
-UTFT lcd1(ST7735, MOSI1, SCLK1, CS1, RESET1, DC1);
-UTFT lcd2(ST7735, MOSI2, SCLK2, CS2, RESET2, DC2);
+UTFT lcd_right(ST7735, 2,1,0,4,3);
+UTFT lcd_left(ST7735, 10,9,8,12,11);
 
+bool write_in_progress = false;
 #define READ_BUFFER_SIZE 200
+char complete_message[READ_BUFFER_SIZE];
 char read_buffer[READ_BUFFER_SIZE];
 int read_buffer_offset = 0;
 int empty_buffer_size = 0;
-
-// constructor args are: DIN, CLK, CS, #CHIPS
-LedControl lc1 = LedControl(4, 3, 2, 1);
-LedControl lc2 = LedControl(7, 6, 5, 1);
+int complete_message_length = 0;
 
 void setupLCD( UTFT &lcd) {
 	lcd.InitLCD(PORTRAIT);
 	lcd.clrScr();
-	lcd.setColor(200, 255, 200); // red, green, blue
+	lcd.setColor(200, 255, 200);
 	lcd.setBackColor(0, 0, 0);
-	lcd.setFont(BigFont); // Allows 15 rows of 20 characters
+	lcd.setFont(SmallFont); // 20 rows; 15 characters
+	lcd.clrScr();
 }
 
 void setup() {
+
+	setupLCD( lcd_right );
+	setupLCD( lcd_left );
+
+	reset_input_buffer();
+	reset_message_buffer();
+	Wire.onReceive(receiveEvent);
 	Wire.begin(SLAVE_HW_ADDRESS);
 
-	setupLCD( lcd1 );
-	setupLCD( lcd2 );
-
-	lc1.shutdown(0, false); // turn off power saving, enables display
-	lc1.setIntensity(0, 15); // sets brightness (0~15 possible values)
-	lc1.clearDisplay(0); // clear screen
-	lc2.shutdown(0, false); // turn off power saving, enables display
-	lc2.setIntensity(0, 15); // sets brightness (0~15 possible values)
-	lc2.clearDisplay(0); // clear screen
 }
 
-void print_lc(LedControl *target, int val) {
-	if (target == NULL) {
-		return;
-	}
-	int digit = 0;
-	bool negative = (val >= 0) ? false : true;
-	val = abs(val);
-	while (val > 0 && digit < 8) {
-		int last_digit = val % 10;
-		val = val / 10;
-		target->setDigit(0, digit, (byte) last_digit, false);
-		digit++;
-	}
-	if (negative) {
-		target->setChar(0, digit, '-', false);
-		digit++;
-	}
-	while (digit < 8) {
-		target->setChar(0, digit, ' ', false);
-		digit++;
-	}
+void print_lcd( UTFT &lcd, int line, const char *str) {
+	lcd.print( str, LEFT, line*10);
 }
 
-void print_lc_string(LedControl *target, const char *str) {
-	int len = strlen(str);
-	int digit = 0;
-	while (digit < 8 && len > 0) {
-		target->setChar(0, digit, str[len - 1], false);
-		len--;
-		digit++;
-	}
+void print_lcd( UTFT &lcd, int line, int number) {
+	char buf[20];
+	sprintf( buf, "%d", number);
+	print_lcd( lcd, line, buf);
 }
 
-void print_lcd1( UTFT &lcd, char *str) {
-	lcd.print(str, LEFT, 0);
-}
-
-void reset_serial_buffer() {
+void reset_input_buffer() {
 	memset(read_buffer, 0, READ_BUFFER_SIZE);
 	read_buffer_offset = 0;
 }
 
+void reset_message_buffer() {
+	memset(complete_message, 0, READ_BUFFER_SIZE);
+	complete_message_length = 0;
+}
+
 void dieError(int number) {
-	char buf[10];
-	sprintf(buf, "%8d", number);
-	print_lc_string(&lc1, "       E");
-	print_lc_string(&lc2, buf);
-// keep hanging around
+	print_lcd( lcd_left, 0, "Error");
+	print_lcd( lcd_left, 1, number);
+	// stay here
 	while (true) {
 	};
 }
 
-/**
- * returns true if buffer contains exactly on line including the "\n"
- * otherwise just stores what is on the serial port and returns false
- */
 bool check_message() {
-// always read one full message
-	if (Wire.available()) {
-		char inByte = 0;
-		while (1) {
-			// just wait for the rest to show up ..
-			while (Wire.available() == 0)
-				;
-			inByte = Wire.read();
-			if (inByte == '\n') {
-				read_buffer[read_buffer_offset] = 0;
-				return true;
-			}
-			if (read_buffer_offset < (READ_BUFFER_SIZE - 1)) {
-				read_buffer[read_buffer_offset] = inByte;
-				read_buffer_offset++;
-			} else {
-				dieError(1);
-			}
-		}
+	if( write_in_progress )
+		return false;
+	if( complete_message[complete_message_length]=='\n' ) {
+		complete_message_length--;
+		complete_message[complete_message_length] = 0;
+		return true;
 	}
 	return false;
 }
 
+// read the data into the buffer,
+// if the current input buffer is not full
+void receiveEvent(int how_many) {
+	write_in_progress = true;
+	while( Wire.available()>0 )
+	{
+		char inByte = Wire.read();
+		// just continue and dump the byte if
+		// we already have a complete buffer and
+		// wait for the app to finish working on the
+		// last one ...
+		if( read_buffer_offset>0 &&
+			read_buffer[read_buffer_offset-1] == '\n' )
+		{
+			continue;
+		}
+		// otherwise store the current byte
+		if (read_buffer_offset < READ_BUFFER_SIZE) {
+			read_buffer_offset++;
+			read_buffer[read_buffer_offset] = inByte;
+		} else {
+			print_lcd( lcd_right, 0, read_buffer);
+			dieError(read_buffer_offset);
+		}
+		if ( inByte == '\n' )
+				break;
+	}
+	// if there is a complete message and the
+	// app says it is done with working on the
+	// last message by setting complete_message[0] to 0
+	// we copy the current message over and reset
+	// the input buffer
+	if( complete_message[0]==0 &&
+		read_buffer[read_buffer_offset] == '\n')
+	{
+		read_buffer[read_buffer_offset+1] = 0;
+		strncpy( complete_message, read_buffer, READ_BUFFER_SIZE);
+		reset_input_buffer();
+		complete_message_length = strlen( complete_message )-1;
+	}
+}
+
 void wait_for_handshake() {
-	reset_serial_buffer();
 	bool dot_on = true;
 	while (true) {
 		if (check_message() == false) {
 			if (dot_on == true) {
-				print_lc_string(&lc1, "        ");
-				print_lc_string(&lc2, "........");
+				print_lcd( lcd_left, 2, ".");
+				print_lcd( lcd_right, 2, " ");
 			} else {
-				print_lc_string(&lc1, "........");
-				print_lc_string(&lc2, "        ");
+				print_lcd( lcd_right, 2, ".");
+				print_lcd( lcd_left, 2, " ");
 			}
 			dot_on = !dot_on;
-			delay(1000);
+			delay(900);
 		} else {
-			StaticJsonBuffer < READ_BUFFER_SIZE > readBuffer;
-			JsonObject& rj = readBuffer.parseObject(read_buffer);
+			print_lcd( lcd_left, 2, " ");
+			print_lcd( lcd_right, 2, " ");
+			StaticJsonBuffer <READ_BUFFER_SIZE> readBuffer;
+			JsonObject& rj = readBuffer.parseObject(complete_message);
 			// the only way to get this thing going
 			if (rj.success() && rj["start"] == 2016) {
-				print_lc_string(&lc1, "       -");
-				print_lc_string(&lc2, "       -");
-				reset_serial_buffer();
+				print_lcd( lcd_left, 1, " OK ");
+				reset_message_buffer();
 				return;
 			}
-			reset_serial_buffer();
+			reset_message_buffer();
 		}
 	}
 }
 
 void check_for_command() {
 	if (check_message() == true) {
-		StaticJsonBuffer < READ_BUFFER_SIZE > readBuffer;
-		JsonObject& rj = readBuffer.parseObject(read_buffer);
-
+		StaticJsonBuffer <READ_BUFFER_SIZE> readBuffer;
+		JsonObject& rj = readBuffer.parseObject(complete_message);
+		reset_message_buffer();
 		if (!rj.success()) {
-			print_lc_string(&lc1, "E");
-			print_lc(&lc2, 3);
+			dieError(3);
 		} else {
-			char buff[20];
+/*			char buff[20];
 			memset(buff, 0, 20);
 			int speed = rj["speed"];
-			print_lc(&lc1, speed);
+			print_lc( lc_top, speed);
 			int height = rj["height"];
-			print_lc(&lc2, height);
-//			snprintf( buff, 15, "%8d", height);
-//			print_lcd1(buff);
+			print_lc( lc_bottom, height);*/
 		}
-		reset_serial_buffer();
 	}
 }
 
 void loop() {
 // main loop
-	check_for_command();
+	wait_for_handshake();
+	while ( 1 )
+		check_for_command();
 }
