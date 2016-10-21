8f60bd900f8c9881bc064a780b1dda66e165af24
Michael Kluge <michael.kluge@tu-dresden.de>
2016-10-21 20:19:33 +0200

    eclipse-zeug

--- Release/core/hooks.c.d
+++
@@ -1,2 +0,0 @@
-core/hooks.c.o: \
- C:/Program\ Files/eclipseArduino/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/hooks.c
--- Release/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-core/new.cpp.o: \
- C:/Program\ Files/eclipseArduino/arduinoPlugin/packages/arduino/hardware/avr/1.6.11/cores/arduino/new.cpp
