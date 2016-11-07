cc5e44ac8d49564b2041e9693a2e6a6961179a1a
Michael Kluge <michael.kluge@tu-dresden.de>
2016-11-07 15:44:52 +0100

    generates correct code now

---
+++ python-code/generate_defines.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+PATH_TO_KSP_DISPLAY="../../ksp_main_controller/src"
+PYTHON_INSTALL="/usr/include/python2.7"
+
+cp $PATH_TO_KSP_DISPLAY/ksp_display_defines.h .
+swig -python -module ksp_console ksp_display_defines.h
+gcc -c -fpic ksp_display_defines_wrap.c -o ksp_display_defines_wrap.o -I$PYTHON_INSTALL -I.
+gcc -shared ksp_display_defines_wrap.o -o _ksp_console.so -lpython2.7
