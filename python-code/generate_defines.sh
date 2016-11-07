0137ea627fa10c675dbe2ef8df79f59c3f9125b4
Michael Kluge <michael.kluge@tu-dresden.de>
2016-11-07 17:29:19 +0100

    windows ... so files are dlls here

--- python-code/generate_defines.sh
+++ python-code/generate_defines.sh
@@ -7,4 +7,4 @@ PYTHON_INSTALL="/usr/include/python2.7"
 cp $PATH_TO_KSP_DISPLAY/ksp_display_defines.h .
 swig -python -module ksp_console ksp_display_defines.h
 gcc -c -fpic ksp_display_defines_wrap.c -o ksp_display_defines_wrap.o -I$PYTHON_INSTALL -I.
-gcc -shared ksp_display_defines_wrap.o -o _ksp_console.so -lpython2.7
+gcc -shared ksp_display_defines_wrap.o -o _ksp_console.dll -lpython2.7
